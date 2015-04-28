dc070ddb9efd70a7502d326abec565b282c8b07a
ihomway <ihomway@live.com>
2015-04-28 12:00:06 +0800

    Initial Commit

---
+++ HCImagePicker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HCImagePicker
+//
+//  Created by Homway on 4/28/15.
+//  Copyright (c) 2015 HC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
